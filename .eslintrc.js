ad1217b61db0e5937123ff9ccb5af906d7a33945
Bohdan Voilov <40231933+b-voilov@users.noreply.github.com>
2023-04-03 23:08:39 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true
+    },
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        quotes: ["error", "double"]
+    }
+}
